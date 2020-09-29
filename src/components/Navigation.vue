c049ddebb29e81ed46cda04350861113e23194d9
Richard Dobson <richard.arthur.dobson@gmail.com>
2020-09-29 20:51:32 +1000

    setup

---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">TriggerPoint.Pro</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/sessions">sessions</router-link>
+          <router-link class="nav-item nav-link" to="/login">log in</router-link>
+          <router-link class="nav-item nav-link" to="/register">register</router-link>
+          
+        </div>
+      </div>
+    </nav>
+
+
+  </div>
+</template>
